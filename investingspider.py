c65b5d6ebdf4ba0ad1ac91c66be22b408d406447
zhangyang <gcmy@whu.edu.cn>
2017-11-14 18:31:16 +0800

    investingspider

---
+++ investingspider.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import datetime
+import pandas as pd
+from mongo import ToolMongoClient
+
+class fx678():
+    def __init__(self,**kwargs):
+        self._biz_mongo_client = ToolMongoClient(kwargs.get("base_cfg_file"))
+        self._ta_collection_name = kwargs.get("_ta_collection_name")
+        self.fx618_new = kwargs.get("fx618_new")
+        self.today = datetime.datetime(2017,3,9,10,10)
